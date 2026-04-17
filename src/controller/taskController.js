586060a150a8c6651ecbabf51c1664f25aae904f
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 10:41:31 +0500

    feat: add function for creating task  objects

---
+++ src/controller/taskController.js
@@ -0,0 +1,18 @@
+import { Task } from "../render/mediator.js";
+
+const createTask = function createTaskObject(title, desc, dateToComplete, priority, note, id) {
+    const newTask = new Task(title, desc, dateToComplete, priority, note, id);
+    return newTask;   
+};
+
+export const addNewTask = function addNewTaskToTheProject(projectToAdd, projectIdToAdd, title, desc, dateToComplete, priority, note, id) {
+    let foundedProject;
+    for (const folder of navMenu) {
+        for (const project of folder.projects) {
+            if (project.title === projectToAdd && project.id === projectIdToAdd) foundedProject = project;
+        }
+    }
+
+    foundedProject.tasks.push(createTask(title, desc, dateToComplete, priority, note, id));
+};
+
