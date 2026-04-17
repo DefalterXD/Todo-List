a6b42b644c2203bee727ca72968574e35c766d81
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:20:03 +0500

    feat: add remove function for deleting a project  element from array

--- src/controller/projectController.js
+++ src/controller/projectController.js
@@ -12,3 +12,15 @@ export const addNewProject = function addNewProjectToTheFolder(folderToAdd, fold
     foundedFolder.projects.push(createProject(title, color, id));
 };
 
+export const editProject = function editProjectProperties(targetProject, newTitle, newMarker) {
+    const foundedProject = navMenu.map((folder) => folder.projects.find((project) => project.id === targetProject.id)).find((project) => project);
+
+    foundedProject.title = newTitle;
+    foundedProject.color = newMarker;
+};
+
+export const removeProject = function removeProjectFromTheFolder(targetProject) {
+    for (const folder of navMenu) {
+        folder.projects = folder.projects.filter((project) => project.id !== targetProject.id);
+    }
+};
