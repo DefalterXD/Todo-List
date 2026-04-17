3d715470844925f92ae9a61571bf50326f150457
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:14:18 +0500

    feat: add function for creating task elements for DOM content

--- src/render/contentRender.js
+++ src/render/contentRender.js
@@ -1,7 +1,15 @@
-import { Element, Icon, Task } from "./mediator.js";
+import { Element, Icon, Task, Inbox, Today, Upcoming, Checklist } from "./mediator.js";
 import { navMenu } from "../controller/menuController.js";
 import { createTaskElement } from "../render/taskRender.js";
 
+
+const findSelectedProject = function findSelectedProjectFromId(selectedProject) {
+    for (const folder of navMenu) {
+        for (const project of folder.projects) {
+            if (project.id === selectedProject) return project;
+        }
+    }
+};
 export const createContentHeader = function createContentHeaderOfTheProject(selectedProject) {
     const project = findSelectedProject(selectedProject);
     const elSection = new Element('section', '', 'section').createElementDOM();
@@ -19,5 +27,14 @@ export const createContentHeader = function createContentHeaderOfTheProject(sele
     headerMetaInfoContainer.append(headerFolderTitle, headerEditButton);
     elSection.append(headerMetaInfoContainer, ulTaskList, addTaskButton);
     return elSection;
+};
+
+export const renderExistingProjectTasks = function renderExistingProjectTasks(selectedProject) {
+    const project = findSelectedProject(selectedProject);
+    const latestSection = document.querySelector('section ul');
+    for (const task of project.tasks) {
+        createTaskElement(task.title, task.desc, task.dateToComplete, task.priority, task.note, task.id, task.status);
+        latestSection.appendChild(Task.latestTaskElement);
+    }
 };
 
