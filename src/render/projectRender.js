0d1138bb05bf3e8f963ea7efda7389084cbc5526
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 23:04:05 +0500

    fix: add remove functions for removing existing DOM project element

--- src/render/projectRender.js
+++ src/render/projectRender.js
@@ -2,18 +2,18 @@ import { Element, Project } from "./mediator.js";
 
 const findProjectList = function findProjectListFromDOM(projectName) {
     return [...document.querySelectorAll('.todo')].find((project) => project.id === projectName.id);
-}
+};
 
 const findProjectTitle = function findProjectTitleFromDOM(projectName) {
     return [...document.querySelectorAll('.section')].find((project) => project.id === projectName.id).querySelector('.header__meta h1');
-}
+};
 
 export const createProjectElement = function createProjectElementForDOM(title, color, id) {
     const elLi = new Element('li', title, 'todo',).createElementDOM();
     elLi.id = id;
     elLi.style.setProperty('--marker-color', color);
     Project.latestProjectElement = elLi;
-}
+};
 
 export const editCreatedProjectElement = function editCreatedProjectElementFromDOM(targetProjectEl, title, marker) {
     const projectList = findProjectList(targetProjectEl);
@@ -22,5 +22,12 @@ export const editCreatedProjectElement = function editCreatedProjectElementFromD
 
     const projectTitle = findProjectTitle(targetProjectEl);
     projectTitle.textContent = title;
-}
+};
 
+export const removeCreatedProjectElement = function removeCreatedProjectElementFromDOM(targetProjectEl) {
+    const projectList = findProjectList(targetProjectEl);
+    projectList.remove();
+
+    const projectContent = findProjectTitle(targetProjectEl).closest('section');
+    projectContent.remove();
+};
