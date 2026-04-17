14f1447d9a084b5145957428982b461062eb6a63
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:28:01 +0500

    feat: add methods for each categories to create and append into DOM

---
+++ src/controller/dialogController.js
@@ -0,0 +1,69 @@
+import { Folder, Project, Task } from "../render/mediator.js";
+import { addNewFolder, editFolder, removeFolder } from "./folderController.js";
+import { createFolderElement, editCreatedFolderElement, removeCreatedFolderElement } from "../render/folderRender.js";
+import { updateFolders } from "./menuController.js";
+import { addNewProject, editProject, removeProject } from "./projectController.js";
+import { createProjectElement, editCreatedProjectElement, removeCreatedProjectElement } from "../render/projectRender.js";
+import { updateProjects } from "./menuController.js";
+import { addNewTask, editTask, removeTask } from "./taskController.js";
+import { createTaskElement, editCreatedTaskElement, removeCreatedTaskElement } from "../render/taskRender.js";
+import { updateTasks } from "./menuController.js";
+
+const creatingObjectOnType = {
+    'Folder': Folder,
+    'Project': Project,
+    'Task': Task
+};
+
+const folderMethods = {
+    createObject(value) {
+        addNewFolder(...value);
+    },
+    createElementDOM(value) {
+        createFolderElement(...value);
+    },
+    updateElements() {
+        updateFolders();
+    },
+};
+
+Object.assign(Folder.prototype, folderMethods);
+
+const projectMethods = {
+    createObject(value) {
+        addNewProject(Project.folderSrc, Project.folderSrcId, ...value);
+    },
+    createElementDOM(value) {
+        createProjectElement(...value);
+    },
+    updateElements() {
+        updateProjects();
+    },
+};
+
+Object.assign(Project.prototype, projectMethods);
+
+const taskMethods = {
+    createObject(value) {
+        addNewTask(Task.projectSrc, Task.projectSrcId, ...value);
+    },
+    createElementDOM(value) {
+        createTaskElement(...value);
+    },
+    updateElements() {
+        updateTasks();
+    },
+    editObject(targetTask, value) {
+        editTask(targetTask, ...value);
+    },
+};
+
+Object.assign(Task.prototype, taskMethods);
+
+export const addNewElement = function addNewElementDependingOnDialogType(elType, ...values) {
+    const newElement = new creatingObjectOnType[elType];
+    newElement.createObject(values);
+    newElement.createElementDOM(values);
+    newElement.updateElements();
+};
+
