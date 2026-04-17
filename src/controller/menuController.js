d32739dbf1dad9e5e3813f79b738ef573a98e029
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:08:38 +0500

    feat: add function for finding created project to render in DOM

--- src/controller/menuController.js
+++ src/controller/menuController.js
@@ -2,6 +2,11 @@ import { Folder, Project, Task } from "../render/mediator.js";
 import { navContainer } from "../render/headerRender.js";
 export let navMenu = [];
 
+const findFolder = function findFolderThroughSrc(folderName, folderId) {
+    const result = [...document.querySelectorAll('.project__title .edit__folder')]
+    .find((folder) => folder.firstChild.textContent === folderName && folder.closest('ul').id === folderId);
+    return result;
+};
 
 export const updateFolders = function updateFoldersAppendingToTheDOM() {
     navContainer.appendChild(Folder.latestFolderElement);
