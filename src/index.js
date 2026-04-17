985a3396fd3db12d0ef5a443a4df97610dbdaef4
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:41:46 +0500

    feat: append created header into body

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./reset.css";
 import "./style.css";
+import { headerContainer } from "./render/headerRender.js";
+import { elementDOMForAppend } from "./render/mediator.js";
+
+elementDOMForAppend.body.prepend(headerContainer);
+
