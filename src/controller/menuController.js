0b335f18f38f7a5716cc2b64617bf9a4c8ccaecc
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:05:46 +0500

    feat: add function for updating any folder changes in DOM

--- src/controller/menuController.js
+++ src/controller/menuController.js
@@ -1,2 +1,9 @@
+import { Folder, Project, Task } from "../render/mediator.js";
+import { navContainer } from "../render/headerRender.js";
 export let navMenu = [];
+
+
+export const updateFolders = function updateFoldersAppendingToTheDOM() {
+    navContainer.appendChild(Folder.latestFolderElement);
+};
 
