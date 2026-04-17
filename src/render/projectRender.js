6055e6ef18dfb08e486de000e13ebd483ff692ce
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 22:37:02 +0500

    feat: add edit function for applying modification of project properties into DOM

--- src/render/projectRender.js
+++ src/render/projectRender.js
@@ -1,10 +1,26 @@
 import { Element, Project } from "./mediator.js";
 
+const findProjectList = function findProjectListFromDOM(projectName) {
+    return [...document.querySelectorAll('.todo')].find((project) => project.id === projectName.id);
+}
+
+const findProjectTitle = function findProjectTitleFromDOM(projectName) {
+    return [...document.querySelectorAll('.section')].find((project) => project.id === projectName.id).querySelector('.header__meta h1');
+}
 
 export const createProjectElement = function createProjectElementForDOM(title, color, id) {
     const elLi = new Element('li', title, 'todo',).createElementDOM();
     elLi.id = id;
     elLi.style.setProperty('--marker-color', color);
     Project.latestProjectElement = elLi;
+}
+
+export const editCreatedProjectElement = function editCreatedProjectElementFromDOM(targetProjectEl, title, marker) {
+    const projectList = findProjectList(targetProjectEl);
+    projectList.textContent = title;
+    projectList.style.setProperty('--marker-color', marker);
+
+    const projectTitle = findProjectTitle(targetProjectEl);
+    projectTitle.textContent = title;
 }
 
