794689c60b2aa2c27dfc9bf1a1c84f0b8670c65b
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-17 10:38:55 +0500

    feat: add function for creating project objects

---
+++ src/controller/projectController.js
@@ -0,0 +1,13 @@
+import { Project } from "../render/mediator.js";
+
+const createProject = function createProjectObject(title, color, id) {
+    const newProject = new Project(title, color, id);
+    return newProject;
+};
+
+
+export const addNewProject = function addNewProjectToTheFolder(folderToAdd, folderIdToAdd, title, color, id) {
+    const foundedFolder = navMenu.find((folder) => folder.title === folderToAdd && folder.id === folderIdToAdd);
+    foundedFolder.projects.push(createProject(title, color, id));
+};
+
