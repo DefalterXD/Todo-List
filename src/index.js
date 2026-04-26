06b8f8fae67317673bfc298cd026c6309c1c5ac4
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-04-26 10:50:25 +0500

    fix: render the tasks on all projects when you completed it

--- src/index.js
+++ src/index.js
@@ -203,6 +203,8 @@ elementDOMForAppend.content.addEventListener('click', (e) => {
 
 elementDOMForAppend.content.addEventListener('input', (e) => {
     changeStatus(e.target.closest('li').id, e.target.checked);
+    const currentSelectedProject = document.querySelector('.selected');
+    checkSelectedFolder(currentSelectedProject);
     getAllFolders();
 });
 
